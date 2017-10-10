b87e74cabe568fd8a80c1310442d634f59e660f5
Belpois Nicolas <nicolasbelpois@gmail.com>
2017-10-10 10:43:27 +0200

    Exo 1
    Compréhension des variables en .js

---
+++ Exo 1/exo1.js
@@ -0,0 +1,30 @@
+//Exercice : Diseur de bonne aventure
+var childs = 2;
+var women = "Erika";
+var country = "Péru";
+var job = "Codeur";
+console.log("Vous serez "+job+" et habiterez au "+country+", et marié a "+women+" avec "+childs+" enfants.");
+
+//Exercice : Calculer l'age
+var year = 2017;
+var born =1990;
+console.log(year-born);
+
+//Exercice : Bientôt vieux
+var age = 27
+var maxage = 100
+var denree = "ice-tea"
+var consom = 1
+var resultat = (maxage-age)*(consom*365)
+console.log("Il vous reste "+resultat+" de "+denree+" avant d'ateindre l'age de "+maxage+" ans");
+
+//Exercice : Priorités
+var result = (1 + 2) * 3 + 4 / 2;
+var etape1 = "(1 + 2) * 3 + 4 / 2";
+var etape2 ="3 * 3 + 4 / 2";
+var etape3 ="9 + 4 / 2";
+var etape4 ="9 + 2";
+console.log(etape1);
+console.log(etape2);
+console.log(etape3);
+console.log(etape4);
