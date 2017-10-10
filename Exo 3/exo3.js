0a84953bfa75d9adffd7136d261a523961992668
Belpois Nicolas <nicolasbelpois@gmail.com>
2017-10-10 15:50:04 +0200

    Exo 3 Wip

---
+++ Exo 3/exo3.js
@@ -0,0 +1,13 @@
+//Exercice : Calculs
+var sous = function (a, b) {
+    return a - b;
+}
+var divi = function (a, b) {
+    return a / b;
+}
+var multi = function (a, b) {
+    return a * b ;
+}
+var pourc = function (a, b) {
+    return ((a * 100) / b)
+}
